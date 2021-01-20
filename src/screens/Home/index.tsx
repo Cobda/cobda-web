2a91385e8b650e46eef12d71bb53891cb05d2638
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-01-20 21:22:46 +0700

    [ch60] Add functions for responsive layout

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
 import './styles.scss';
 
 export default function Home() {
-  return <div className='root'>Hello World</div>;
+  return (
+    <div className='root'>
+      <Navbar />
+      <Sidebar />
+      <Footer />
+    </div>
+  );
 }
