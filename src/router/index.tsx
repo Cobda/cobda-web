1566971f2161fc84e53dc99f7d962392acbde4fc
Raksani <raksani.k@ku.th>
2021-01-22 22:05:48 +0700

    [ch64] Create about us screen.

---
+++ src/router/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import AboutUs from '../screens/AboutUs';
+import Home from '../screens/Home';
+import PageNotFound from '../screens/PageNotFound';
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/aboutus">
+          <AboutUs/>
+        </Route>
+        <Route path="/404">
+          <PageNotFound />
+        </Route>
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
+  );
+}
