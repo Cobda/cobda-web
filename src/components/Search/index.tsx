37ec86389a27a98161debc697fce6225883e623b
Raksani <raksani.k@ku.th>
2021-01-22 23:03:40 +0700

    [ch68] Add Search component consists of search button

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles.scss';
+
+export default function Search() {
+  return (
+    <div className="search-container">
+        <Link to="/product-list" className="btn btn-search">Search</Link>
+    </div>
+  );
+}
+
+
+
+
