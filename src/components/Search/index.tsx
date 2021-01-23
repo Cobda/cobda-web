3117838554c5ef8a0383047a6a00d08d13cd463f
Raksani Khunamas <raksani.k@ku.th>
2021-01-23 20:40:28 +0700

    [ch68] Rename function 'Search' to 'HomePageSearch'

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './styles.scss';
 
-export default function Search() {
+export default function HomePageSearch() {
   return (
     <div className="search-container">
       <Link to="/product-list" className="btn btn-search">Search</Link>
     </div>
   );
 }
-
 
 
