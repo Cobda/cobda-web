4d31c3f064900b07a5caa5dd5b74537e2b2c5184
Raksani <raksani.k@ku.th>
2021-01-23 22:02:03 +0700

    [ch68] Update search button to approach BEM SCSS style

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 export default function HomePageSearch() {
   return (
-    <div className="search-container">
-      <Link to="/product-list" className="btn btn-search">Search</Link>
+    <div className="search">
+      <Link to="/product-list" className="search__button">
+        <div className="search__text">Search</div>
+      </Link>
     </div>
   );
 }
