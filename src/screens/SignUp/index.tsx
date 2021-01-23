060a35c0c3e2b2ff5ef516683a9085c83f46df66
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-01-23 14:19:12 +0700

    [ch70] Create and route to SignUp screen

---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
+import './styles.scss';
+
+export default function SignUp() {
+    return (
+      <>
+        <header>
+          <Navbar />
+        </header>
+      </>
+    );
+  }
