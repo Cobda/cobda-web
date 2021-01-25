7dc39693d2bd2dbc4ff44ae8dbe085be9d6d6541
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-01-25 18:23:23 +0700

    [ch65] Refine styles

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import SignUpForm from '../../components/SignUpForm';
+import SignUpForm from "../../components/SignUpForm";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
 import "./styles.scss";
 
 export default function SignUp() {
   return (
     <>
-      <div className="container">
+      <header>
+        <Navbar />
+      </header>
+      <main className="sign-up">
         <SignUpForm />
-      </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
