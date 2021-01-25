43bacb541083ba6dfc36b617934cead4ea759419
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-01-25 18:50:22 +0700

    [ch65] Fix typo

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./styles.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
 export default function SignUpForm() {
   return (
--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import SignUpForm from "../../components/SignUpForm";
-import Footer from "../../components/Footer";
-import Navbar from "../../components/Navbar";
-import "./styles.scss";
+import React from 'react';
+import SignUpForm from '../../components/SignUpForm';
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
+import './styles.scss';
 
 export default function SignUp() {
   return (
