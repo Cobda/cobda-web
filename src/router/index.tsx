936e2ccb2258307176f47c8d7e16d486361d7653
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-01-25 22:56:36 +0700

    [ch85] Test adding class to body tag

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,6 +9,10 @@ import Home from '../screens/Home';
 import PageNotFound from '../screens/PageNotFound';
 
 export default function Routes() {
+  useEffect(() => {
+    console.log('Body: ', document.body.classList.add('Test'));
+  });
+
   return (
     <Router>
       <Switch>
