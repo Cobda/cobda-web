8f7215616f081f5df5046aac13383c8a72ed873a
bankkeez <sethitikul@gmail.com>
2021-01-26 13:16:13 +0700

    [ch85] Refine codes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Footer from '../../components/Footer';
 import HomeSection from '../../components/HomeSection';
 import Navbar from '../../components/Navbar';
