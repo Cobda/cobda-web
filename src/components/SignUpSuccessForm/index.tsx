e6738ab35144e672ad72457db7b20c11cb07745d
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-01-28 20:33:39 +0700

    [ch70] Refine layout for sign up succees form

--- src/components/SignUpSuccessForm/index.tsx
+++ src/components/SignUpSuccessForm/index.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const SignUpSuccessForm = () => {
   return (
-    <div className="sign-up-success-form">
-      <h2>Registration Success</h2>
-    </div>
+    <section className="sign-up-success-form">
+      <header className="sign-up-success-form__header">Sample Picture</header>
+      <div className="sign-up-success-form__body">
+        <h2>Registration Success</h2>
+      </div>
+      <footer className="sign-up-success-form__footer">
+        <div className="form__actionable">
+          <button className="form__button">Continue</button>
+        </div>
+      </footer>
+    </section>
   )
 }
 
