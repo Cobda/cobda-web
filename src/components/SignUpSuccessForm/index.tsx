641ce192dcc9294a661c422ecdf8260071601ad6
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-01-29 15:58:55 +0700

    [ch70] Refine sample image

--- src/components/SignUpSuccessForm/index.tsx
+++ src/components/SignUpSuccessForm/index.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const SignUpSuccessForm = () => {
   return (
     <section className="sign-up-success-form">
-      <div className="sign-up-success-form__image">Sample Picture</div>
+      <div className="sign-up-success-form__box">
+        <div className="sign-up-success-form__image">Sample Picture</div>
+      </div>
       <header className="sign-up-success-form__header">
         <h2>Registration Success</h2>
       </header>
