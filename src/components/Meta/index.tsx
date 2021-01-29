566c690518d3ffa31a06f4cbac4c9184d7338323
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-01-29 16:45:34 +0700

    [ch106] Init meta component

---
+++ src/components/Meta/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+
+interface Meta {
+  title?: string
+  keywords?: string
+  description?: string
+}
+
+const Meta = ({ title, keywords, description }: Meta) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>{title}</title>
+    </Head>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Cobda: Online Marketplace for Streetwear Products',
+  keywords:
+    'cobda, cobda.com, buy, sell, marketplace, streetwear, vintage, sneakers, shirts',
+  description: 'Buy and Sell streetwear products including sneakers and shirts',
+}
+
+export default Meta
