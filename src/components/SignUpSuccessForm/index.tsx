a364b62aa68315fc3f9651859b6f9ab261a07dfc
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-01-29 16:28:57 +0700

    [ch70] Rename css class name for better meaning

--- src/components/SignUpSuccessForm/index.tsx
+++ src/components/SignUpSuccessForm/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SignUpSuccessForm = () => {
   return (
     <section className="sign-up-success-form">
-      <div className="sign-up-success-form__box">
+      <div className="sign-up-success-form__image-container">
         <div className="sign-up-success-form__image">Sample Picture</div>
       </div>
       <header className="sign-up-success-form__header">
