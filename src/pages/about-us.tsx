726da3250cbab61033e93b9f1a1f6df6094671d2
Raksani <raksani.k@ku.th>
2021-01-31 17:28:37 +0700

    [ch64] Add 'about us' text

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,16 +1,18 @@
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const AboutUs = () => (
-    <div className="layout-about-us">
-        <header>
-            <Navbar />
-        </header>
-        <main>
-        </main>
-        <footer>
-            <Footer />
-        </footer>
-    </div>
-)
-export default AboutUs
+  <div className="layout-about-us">
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <div>About Us</div>
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+  </div>
+);
+
+export default AboutUs;
