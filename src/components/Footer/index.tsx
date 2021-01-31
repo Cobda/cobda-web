d546cf60cce313e70bdd79e3de49abccd61ecbbe
Raksani <raksani.k@ku.th>
2021-01-31 17:25:44 +0700

    [ch68] Fix Footer link and classname

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 
 const Footer = () => (
-  <div className="footer-container">
+  <div className="footer">
     Footer
     <div className="footer__item">
-      <Link href="/about-us">About Us</Link>
+      <Link href="/about-us">
+        <a className="footer__link">About Us</a>
+      </Link>
     </div>
   </div>
 )
