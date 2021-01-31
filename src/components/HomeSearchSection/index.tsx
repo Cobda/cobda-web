42d96aaa8654398d3ff3e5d64a634a43c50da66f
Raksani <raksani.k@ku.th>
2021-01-31 16:50:45 +0700

    [ch68] Merge and fix home-search button

--- src/components/HomeSearchSection/index.tsx
+++ src/components/HomeSearchSection/index.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
 export default function HomeSearchSection() {
-  const handleSearchClick = () => {};
+  const router = useRouter();
+
+  const handleSearchClick = () => {
+    router.push("/product-list");
+  };
 
   return (
     <section className="home-section">
       <header className="home-section__header">
-        <h2 className="home-section__title">Search section</h2>
-        <p className="home-section__description">This is a search section</p>
-        <button className="home-search-tab__button" onClick={handleSearchClick}>
-          Search
-        </button>
+        <h2 className="home-section__title">Sample 1</h2>
+        <p className="home-section__description">
+          This is a sample description
+        </p>
       </header>
+      <div className="home-search">
+        <button className="home-search__button" onClick={handleSearchClick}>
+          <div className="home-search__text">Search</div>
+        </button>
+      </div>
     </section>
   );
 }
