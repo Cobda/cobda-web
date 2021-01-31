ce28833a84dafc7620e5321c431cad01471e135f
Raksani <raksani.k@ku.th>
2021-01-31 14:30:19 +0700

    [ch64] Add About Us screen and layout style

---
+++ src/pages/about-us.tsx
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+const AboutUs = () => (
+    <div className="layout-about-us">
+        <header>
+            <Navbar />
+        </header>
+        <main>
+        </main>
+        <footer>
+            <Footer />
+        </footer>
+    </div>
+)
+export default AboutUs
