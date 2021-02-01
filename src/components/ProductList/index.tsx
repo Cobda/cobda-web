9aec2f669129cb78dccb9b85460a5d8391a4745c
Raksani <raksani.k@ku.th>
2021-02-01 18:10:54 +0700

    [ch68] Add product list component

---
+++ src/components/ProductList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <section className="product-list">
+      <h2 className="product-list__title">Product List</h2>
+      <p className="product-list__description">This is a sample description</p>
+    </section>
+  )
+}
+
+export default ProductList
