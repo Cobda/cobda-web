d36a22da055d2f45a4491f9de49abf84c2edec93
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-02-01 15:14:01 +0700

    [ch108] Bemify css then add avatar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,71 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-const Navbar = () => {
+interface Navbar {
+  isLogin?: boolean
+}
+
+const Navbar = ({ isLogin }: Navbar) => {
+  const router = useRouter()
+
+  const handleSignInClick = () => {
+    // TODO: Open SignIn modal
+  }
+
+  const handleSignUpClick = () => {
+    router.push('/sign-up')
+  }
+
+  const handleAvatarClick = () => {
+    // TODO: Expand dropdown or sth
+  }
+
   return (
     <nav className="navbar">
-      <h2 className="navbar__title">COBDA</h2>
-      <Link href="/sign-up">
-        <a className="navbar__item">Sign Up</a>
-      </Link>
+      <div className="navbar__brand">
+        {/* TODO: Navbar brand logo */}
+        <span className="navbar__brand-name">COBDA</span>
+      </div>
+      <ul className="navbar__menu">
+        <li className="navbar__menu-item">
+          <Link href="/">
+            <a className="navbar__link">Home</a>
+          </Link>
+        </li>
+        <li className="navbar__menu-item">
+          <Link href="/product-list">
+            <a className="navbar__link">Products</a>
+          </Link>
+        </li>
+        <li className="navbar__menu-item">
+          <Link href="/about-us">
+            <a className="navbar__link">About Us</a>
+          </Link>
+        </li>
+      </ul>
+      <div className="navbar__authentication">
+        {isLogin ? (
+          <button
+            className="navbar__avatar-button"
+            onClick={handleAvatarClick}>
+            <img src="#" alt="avatar" />
+          </button>
+        ) : (
+          <>
+            <button
+              className="navbar__authentication-button"
+              onClick={handleSignInClick}>
+              Sign In
+            </button>
+            <button
+              className="navbar__authentication-button"
+              onClick={handleSignUpClick}>
+              Sign Up
+            </button>
+          </>
+        )}
+      </div>
     </nav>
   )
 }
