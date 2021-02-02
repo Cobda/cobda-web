2457e1993a762dbced780ca57b0073b68b4fc224
Raksani <raksani.k@ku.th>
2021-02-03 00:18:32 +0700

    [ch64] Refine code and rename button

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const ProductList = () => {
   return (
-    <div>
       <section className="product-list">
         <header>
           <h2 className="product-list__title">Product List</h2>
@@ -11,7 +10,6 @@ const ProductList = () => {
           </p>
         </header>
       </section>
-    </div>
   )
 }
 
