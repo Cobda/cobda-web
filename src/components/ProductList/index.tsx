baec9846a8560327008031f58185451d6c108cbc
Raksani <raksani.k@ku.th>
2021-02-04 18:48:04 +0700

    [ch69] Edit link to detail page

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const ProductList = () => {
-  const router = useRouter()
-
-  const handleClick = () => {
-    router.push('/product-detail')
-  }
-
   return (
     <section className="product-list">
       <header>
-        <h2 className="product-list__title" onClick={handleClick}>
-          Product List
+        <h2 className="product-list__title">
+          <Link href="/product-detail">
+            <a className="product-list__link-to-detail">Product List</a>
+          </Link>
         </h2>
         <p className="product-list__description">
           This is a sample description
