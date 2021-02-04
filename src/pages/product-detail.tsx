61b2c8912e4342ef329c296c9361d246d7b6a0d7
Raksani <raksani.k@ku.th>
2021-02-04 18:37:18 +0700

    [ch69] Fix layout

--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -1,5 +1,6 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import ProductDetailComponent from '../components/ProductDetail'
 
 const ProductDetail = () => (
   <div className="layout-product-detail">
@@ -7,23 +8,7 @@ const ProductDetail = () => (
       <Navbar />
     </header>
     <main>
-      <div className="content-1">
-        <div className="product-detail-section__image-container">
-          <div className="product-detail-section__image">Sample Picture</div>
-        </div>
-      </div>
-      <div className="content-2">
-        <header className="product-detail-section__header">
-          <h2 className="product-detail-section__title">Shoe Title</h2>
-        </header>
-      </div>
-      <div className="content-3">
-      <header className="product-detail__description-container">
-        <h2 className="product-detail__description-text">
-          Product description
-        </h2>
-      </header>
-      </div>
+      <ProductDetailComponent/>
     </main>
     <footer>
       <Footer />
