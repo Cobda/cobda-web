1b65464a7cb519167a28e4c52a81c582383da7af
Raksani <raksani.k@ku.th>
2021-02-04 15:04:26 +0700

    [ch69] Add link on ProductList redirect to ProductDetail page

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const ProductList = () => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/product-detail')
+  }
+
   return (
-      <section className="product-list">
-        <header>
-          <h2 className="product-list__title">Product List</h2>
-          <p className="product-list__description">
-            This is a sample description
-          </p>
-        </header>
-      </section>
+    <section className="product-list">
+      <header>
+        <h2 className="product-list__title" onClick={handleClick}>
+          Product List
+        </h2>
+        <p className="product-list__description">
+          This is a sample description
+        </p>
+      </header>
+    </section>
   )
 }
 
