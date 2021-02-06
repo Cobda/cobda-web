5bb20762b14ab5fcb565481050c7583835988941
bankkeez <sethitikul@gmail.com>
2021-02-06 17:37:52 +0700

    [ch69] Modify ProductInformation

--- src/components/ProductContent/ProductInformation.tsx
+++ src/components/ProductContent/ProductInformation.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 
 const ProductInformation = () => {
   return (
@@ -9,6 +9,23 @@ const ProductInformation = () => {
           This is a sample description
         </p>
       </header>
+      <p className="product-information__description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin iaculis
+        maximus sollicitudin. Donec et orci scelerisque, tempor massa vel,
+        tristique nunc. Maecenas fermentum commodo elit, non efficitur felis
+        dignissim vel. In vitae metus lacus. Pellentesque accumsan vulputate
+        ligula, et finibus dolor sagittis quis. Integer at justo non dui
+        ultricies sodales a sed sem. Vivamus augue metus, dignissim sit amet
+        dolor quis, tempor consequat purus. Etiam tincidunt leo ac turpis
+        aliquet semper. Quisque eu tempus felis, ut blandit ante. Cras auctor
+        est eget elit fringilla porttitor. In at faucibus diam. Proin quis dui
+        iaculis, rutrum erat et, pulvinar arcu. Phasellus massa quam, blandit
+        sed nibh sit amet, ultricies consequat arcu. Phasellus facilisis mattis
+        pellentesque. Aliquam congue arcu a sem interdum convallis. Aenean
+        ullamcorper lacus ipsum, sed vehicula lectus semper et. Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit. Proin eu magna id nibh
+        fermentum varius. Suspendisse potenti.
+      </p>
     </div>
   )
 }
