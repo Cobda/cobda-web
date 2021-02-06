00f0798ae25722dd0f7c76d6c99b0375e6196190
bankkeez <sethitikul@gmail.com>
2021-02-06 17:31:40 +0700

    [ch69] Modify ProductDetail

--- src/components/ProductContent/ProductDetail.tsx
+++ src/components/ProductContent/ProductDetail.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 const ProductDetail = () => {
   return (
-    <section className="product-detail">
+    <div className="product-detail">
       <header className="product-detail__header">
         <h2 className="product-detail__title">Shoe Title</h2>
       </header>
-    </section>
+      <p className="product-detail__description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin libero
+        erat, porttitor mattis turpis eu, fermentum porttitor elit. Integer
+        auctor orci at ligula cursus pellentesque. Pellentesque in eleifend
+        ex. Curabitur bibendum orci vel magna tristique egestas. Aliquam id
+        libero augue. Proin rutrum viverra ex, ac ornare velit. Etiam accumsan
+        massa a ipsum interdum placerat. Vestibulum consequat ligula id diam
+        commodo commodo. Donec tincidunt ipsum dui, eget suscipit lorem
+        maximus sed. Integer nec diam quis erat luctus ultrices sed quis eros.
+      </p>
+    </div>
   )
 }
 
