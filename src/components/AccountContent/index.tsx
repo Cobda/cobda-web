6381ffa995205830245a866cd941450fa2c3092d
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-02-14 00:13:17 +0700

    [ch109] Init high level grid structure

---
+++ src/components/AccountContent/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AccountContent = () => {
+  return <div className="account-content">Account Content</div>
+}
+
+export default AccountContent
---
+++ src/components/AccountInformation/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AccountInformation = () => {
+  return <div className="account-information">Account Information</div>
+}
+
+export default AccountInformation
