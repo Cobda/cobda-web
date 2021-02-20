bdd43ef6b1bc3485b85bae8d3db7efcc2793f688
bankkeez <sethitikul@gmail.com>
2021-02-21 00:52:30 +0700

    [ch139][ch141] Add Category title for Thai

--- src/components/HomeCategorySection/index.tsx
+++ src/components/HomeCategorySection/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import HomeCategory from '../HomeCategory'
+import useTranslation from 'next-translate/useTranslation'
 
 const HomeCategorySection = () => {
+  const { t } = useTranslation('home')
+
   return (
     <section className="home-section home-section--dark">
       <header className="home-section__header">
-        <h2 className="home-section__title">CATEGORIES</h2>
+        <h2 className="home-section__title">{t('categorySectionTitle')}</h2>
       </header>
       <HomeCategory />
     </section>
