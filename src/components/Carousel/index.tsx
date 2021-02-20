0351767054bd9a9ecf6dbb8d2512b489c0c7b96e
bankkeez <sethitikul@gmail.com>
2021-02-20 16:50:00 +0700

    [ch145][ch150] Adjust buttons and carousel items size

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel'
 import CarouselItem from './Item'
-import 'pure-react-carousel/dist/react-carousel.es.css'
 
 const Carousel = () => {
   return (
@@ -10,7 +9,7 @@ const Carousel = () => {
         visibleSlides={3}
         naturalSlideWidth={400}
         naturalSlideHeight={500}
-        totalSlides={9}
+        totalSlides={5}
         infinite={true}
       >
         <Slider>
@@ -29,21 +28,9 @@ const Carousel = () => {
           <Slide index={5}>
             <CarouselItem caption={"5th slide"} />
           </Slide>
-          <Slide index={6}>
-            <CarouselItem caption={"6th slide"} />
-          </Slide>
-          <Slide index={7}>
-            <CarouselItem caption={"7th slide"} />
-          </Slide>
-          <Slide index={8}>
-            <CarouselItem caption={"8th slide"} />
-          </Slide>
-          <Slide index={9}>
-            <CarouselItem caption={"9th slide"} />
-          </Slide>
         </Slider>
-        <ButtonBack className="buttonBack">Back</ButtonBack>
-        <ButtonNext className="buttonNext">Next</ButtonNext>
+        <ButtonBack className="buttonBack">{'<'}</ButtonBack>
+        <ButtonNext className="buttonNext">{'>'}</ButtonNext>
         <DotGroup className="dotGroup" />
       </CarouselProvider>
     </div>
