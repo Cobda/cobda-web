da5fd7be182b36776f13b125c5a4124e3795d129
bankkeez <sethitikul@gmail.com>
2021-02-21 01:56:15 +0700

    [ch139][ch141] Refine codes

--- src/components/HomeCategory/index.tsx
+++ src/components/HomeCategory/index.tsx
@@ -5,21 +5,25 @@ const HomeCategory = () => {
   return (
     <ul className="home-category">
       <HomeCategoryItem
-        imagePath={"/images/yeezy-380.jpg"}
-        alternate={"Adidas Yeezy 380 from Pexels by @ox-street-3848035"}
-        caption={"Yeezys"} />
+        imagePath={'/images/yeezy-380.jpg'}
+        alternate={'Adidas Yeezy 380 from Pexels by @ox-street-3848035'}
+        caption={'Yeezys'}
+      />
       <HomeCategoryItem
-        imagePath={"/images/blue-nike-jordan.jpg"}
-        alternate={"Nike Jordan from Pexels by @desertedinurban"}
-        caption={"Jordans"} />
+        imagePath={'/images/blue-nike-jordan.jpg'}
+        alternate={'Nike Jordan from Pexels by @desertedinurban'}
+        caption={'Jordans'}
+      />
       <HomeCategoryItem
-        imagePath={"/images/rolling-stone-shirt.jpg"}
-        alternate={"One of the Author's vintage t-shirt collection"}
-        caption={"Vintage"} />
+        imagePath={'/images/rolling-stone-shirt.jpg'}
+        alternate={'One of our Authors vintage t-shirt collection'}
+        caption={'Vintage'}
+      />
       <HomeCategoryItem
-        imagePath={"/images/nike-jordan-off-white.jpg"}
-        alternate={"Nike Air Jordan X Off White from Pexels by @introspectivedsgn"}
-        caption={"HypeBeast"} />
+        imagePath={'/images/nike-jordan-off-white.jpg'}
+        alternate={'Nike Air Jordan X Off White from Pexels by @introspectivedsgn'}
+        caption={'HypeBeast'}
+      />
     </ul>
   )
 }
