79f828c64de02fe2a9e9b1f7457f658de7fdccd3
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-02-21 17:25:38 +0700

    [ch121] Init dropdown logic

--- src/components/Navbar/Language.tsx
+++ src/components/Navbar/Language.tsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 const NavbarLanguage = () => {
+  const [open, setOpen] = useState<boolean>(false)
+  const [selectedLocale, setSelectedLocale] = useState<String>('th')
   const router = useRouter()
 
   const handleLocaleClick = (locale: string) => {
     const { pathname, asPath } = router
+    setSelectedLocale(locale)
     router.push(pathname, asPath, { locale })
   }
 
+  const handleArrowClick = () => {
+    setOpen(!open)
+  }
+
   const renderNavbarFlag = () => {
-    const localeLinks = router.locales?.map((locale: string, index: number) => (
-      <a
+    const flagTH = <Image src="/icons/thailand.svg" height={16} width={16} />
+    const flagEN = (
+      <Image src="/icons/united-kingdom.svg" height={16} width={16} />
+    )
+    const localeItems = router.locales?.map((locale: string, index: number) => (
+      <li
+        className="dd-list-item"
         key={index}
-        className="navbar__link navbar__link--locale"
         onClick={() => handleLocaleClick(locale)}>
-        {/* TODO: Please change according to the design */}
-        {locale.toLocaleUpperCase()}
-      </a>
+        {locale === 'th' ? flagTH : flagEN}
+        <span key={index} className="navbar__link navbar__link--locale">
+          {locale.toLocaleUpperCase()}
+        </span>
+      </li>
     ))
-    
-    return localeLinks ? (
-      <div className="navbar__flag">{localeLinks}</div>
+
+    return localeItems ? (
+      <div className="navbar__flag">
+        {selectedLocale === 'th' ? (
+          <Image src="/icons/thailand.svg" height={32} width={32} />
+        ) : (
+          <Image src="/icons/united-kingdom.svg" height={32} width={32} />
+        )}
+        <button onClick={handleArrowClick}></button>
+        {open && <ul className="dd-list">{localeItems}</ul>}
+      </div>
     ) : (
       <></>
     )
