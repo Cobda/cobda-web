a3fc3f711bfcce4a8026319b685246202df9e243
Raksani <raksani.k@ku.th>
2021-02-23 03:20:57 +0700

    [ch131] Modify banner position and decorate

--- src/components/HomeSearchSection/index.tsx
+++ src/components/HomeSearchSection/index.tsx
@@ -12,23 +12,17 @@ const HomeSearchSection = () => {
 
   return (
     <section className="home-search-section">
-      {/* <header className="home-search-section__header"> */}
+      <div className="home-search-section__decorate"></div>
         <div className="home-search-section__image"></div>
         <h2 className="home-search-section__title">Cobda, streetwear community</h2>
         <p className="home-search-section__description">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin libero
         erat, porttitor mattis turpis eu, fermentum porttitor elit. Integer
-        auctor orci at ligula cursus pellentesque. Pellentesque in eleifend
-        ex. Curabitur bibendum orci vel magna tristique egestas. Aliquam id
-        libero augue. Proin rutrum viverra ex, ac ornare velit. Etiam accumsan
-        massa a ipsum interdum placerat. Vestibulum consequat ligula id diam
-        commodo commodo. Donec tincidunt ipsum dui, eget suscipit lorem
-        maximus sed. Integer nec diam quis erat luctus ultrices sed quis eros.
+        auctor orci at ligula cursus pellentesque.
         </p>
-      {/* </header> */}
-      <div className="home-search-box">
-        <button className="home-search-box__button" onClick={handleSearchClick}>
-          <span className="home-search-box__text">Search</span>
+      <div className="home-search-section__searchbox">
+        <button className="home-search-section__searchbox--button" onClick={handleSearchClick}>
+          <span className="home-search-section__searchbox--text">Search</span>
         </button>
       </div>
     </section>
