5e5ac6fc94dd12a69d91a87c75868bd6daec903d
bankkeez <sethitikul@gmail.com>
2021-02-24 18:30:28 +0700

    [ch131] Add Thai context

--- src/components/HomeSearchSection/index.tsx
+++ src/components/HomeSearchSection/index.tsx
@@ -8,12 +8,8 @@ const HomeSearchSection = () => {
     <section className="home-section home-section--background-image">
       <div className="home-search home-search--background">
         <div className="home-search__content">
-          <h1 className="home-search__title">COBDA, PRESENTED BY BANK.</h1>
-          <p className="home-search__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mi
-            magna, placerat eu semper at, pretium sit amet libero. Integer
-            ultrices tristique quam.
-          </p>
+          <h1 className="home-search__title">{t('searchSectionTitle')}</h1>
+          <p className="home-search__description">{t('searchSectionDescription')}</p>
         </div>
       </div>
       {/* TODO: Add search box */}
