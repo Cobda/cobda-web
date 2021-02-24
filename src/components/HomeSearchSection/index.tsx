dd9e04f346ec2c10117c1a1f5db0927d18c4fa1e
bankkeez <sethitikul@gmail.com>
2021-02-24 18:23:58 +0700

    [ch131] Refine codes

--- src/components/HomeSearchSection/index.tsx
+++ src/components/HomeSearchSection/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import { useRouter } from 'next/router'
 import useTranslation from 'next-translate/useTranslation'
 
 const HomeSearchSection = () => {
-  const router = useRouter()
   const { t } = useTranslation('home')
-
-  const handleSearchClick = () => {
-    router.push('/products')
-  }
 
   return (
     <section className="home-section home-section--background-image">
@@ -22,11 +16,7 @@ const HomeSearchSection = () => {
           </p>
         </div>
       </div>
-      {/* <div className="home-search-box">
-        <button className="home-search-box__button" onClick={handleSearchClick}>
-          <span className="home-search-box__text">{t("search")}</span>
-        </button>
-      </div> */}
+      {/* TODO: Add search box */}
     </section>
   )
 }
