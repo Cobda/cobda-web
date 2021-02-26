ffcce75a7f104c4e994e58be021504a578b12d4a
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-02-27 02:31:38 +0700

    [ch133][ch137] Modify double quote image size

--- src/components/QuoteBox/index.tsx
+++ src/components/QuoteBox/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
-
 interface QuoteBox {
   readonly quote: string
 }
 
-const QuoteBox = ({ quote }: QuoteBox) => {
+const QuoteBox = ({ quote }: QuoteBox) => {  
   return (
     <div className="quote-box">
       <header className="quote-box__header">
         <h3 className="quote-box__title">QUOTES</h3>
       </header>
       <div className="quote-box--background-image">
-        {/* TODO */}
-        <p className="quote-box__">{quote}</p>
+        <p className="quote-box__content">{quote}</p>
       </div>
     </div>
   )
