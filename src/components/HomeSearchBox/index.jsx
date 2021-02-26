101067d5644bba1ee3fb8fe40b6323873e80261d
Raksani <raksani.k@ku.th>
2021-02-27 02:42:52 +0700

    [ch135] Add seach action

--- src/components/HomeSearchBox/index.jsx
+++ src/components/HomeSearchBox/index.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from "next/router"
 
 const HomeSearchBox = () => {
   const { t } = useTranslation('home')
+  const [searchItem, setSearch] = useState('');
+
+  const handleClickSearch = (e) => {
+    window.open(`products/${searchItem}`);
+    setSearch("")
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleClickSearch()
+    }
+  }
 
   return (
     <div className="home-search-box">
-      <input type="text" placeholder="What are you looking for?" />
-      <button className="home-search-box__button" />
+      <input
+        value={searchItem}
+        type="text"
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyUp={handleKeyPress}
+        placeholder="What are you looking for?"
+        name="search"
+      />
+      <button data-key="13" onClick={handleClickSearch} className="home-search-box__button" />
     </div>
   )
 }
