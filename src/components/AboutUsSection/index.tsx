24c5802221c4d65b4f9086044fc6ad9152abf466
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-03-03 16:54:11 +0700

    [ch133][ch137] Resolve conflicts after rebase

--- src/components/AboutUsSection/index.tsx
+++ src/components/AboutUsSection/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import AboutUsIntroduction from '../AboutUsIntroduction'
 import AboutUsQuoteSection from '../AboutUsQuoteSection'
 
 const AboutUsSection = () => {
   return (
     <>
-      <section className="about-us-section about-us-section--light">
-        <header className="about-us-section__header">
-          <h2 className="about-us-section__title">Introduction</h2>
-        </header>
-      </section>
+      <AboutUsIntroduction />
       <section className="about-us-section about-us-section--light">
         <header className="about-us-section__header">
           <h2 className="about-us-section__title">Our Goal</h2>
