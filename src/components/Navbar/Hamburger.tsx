e1d795d18ed4a1aacc6ca38caeb33dac0844af51
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-03-04 20:52:33 +0700

    [ch167] Adding hamburger menu design

--- src/components/Navbar/Hamburger.tsx
+++ src/components/Navbar/Hamburger.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
+import NavbarAccount from './Account'
+import { useRouter } from 'next/router'
 
 const Hamburger = () => {
+  const router = useRouter()
+
+  const handleProductClick = () => {
+    router.push('/products')
+  }
+
+  const handleAboutUsClick = () => {
+    router.push('/about-us')
+  }
+
+  const handleSignInClick = () => {
+    router.push('/sign-in')
+  }
+
+  const handleSignUpClick = () => {
+    router.push('/sign-up')
+  }
+
   return (
     <div className="hamburger">
       <input type="checkbox" className="hamburger__input" />
@@ -8,12 +29,18 @@ const Hamburger = () => {
       <span></span>
       <span></span>
       <ul className="hamburger__menu">
-        <li className="hamburger__menu-list">Mock up</li>
-        <li className="hamburger__menu-list">Mock up</li>
-        <li className="hamburger__menu-list">Mock up</li>
-        <li className="hamburger__menu-list">Mock up</li>
-        <li className="hamburger__menu-list">Mock up</li>
-        <li className="hamburger__menu-list">Mock up</li>
+        <li className="hamburger__menu-list">
+          <a onClick={handleProductClick}>Products</a>
+        </li>
+        <li className="hamburger__menu-list">
+          <a onClick={handleAboutUsClick}>About Us</a>
+        </li>
+        <li className="hamburger__menu-list">
+          <a onClick={handleSignInClick}>Sign In</a>
+        </li>
+        <li className="hamburger__menu-list">
+          <a onClick={handleSignUpClick}>Sign Up</a>
+        </li>
       </ul>
     </div>
   )
