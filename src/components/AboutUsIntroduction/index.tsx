7845b22d039fbd8f94884d1f5e519f96584512cd
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-03-09 16:51:30 +0700

    [ch142] Refine code naming convention

--- src/components/AboutUsIntroduction/index.tsx
+++ src/components/AboutUsIntroduction/index.tsx
@@ -4,12 +4,12 @@ import Trans from 'next-translate/Trans'
 
 const AboutUsIntroduction = () => {
   const { t } = useTranslation('about-us')
-  const titles = [<h1 className="about-us-introduction__title" />, <br />]
+  const title = [<h1 className="about-us-introduction__title" />, <br />]
 
   return (
     <section className="about-us-section">
       <div className="about-us-introduction about-us-introduction--background">
-        <Trans i18nKey={t('introductionSectionTitle')} components={titles} />
+        <Trans i18nKey={t('introductionSectionTitle')} components={title} />
         <p className="about-us-introduction__description">
           {t('introductionSectionDescription')}
         </p>
--- src/components/NewReleaseBanner/index.tsx
+++ src/components/NewReleaseBanner/index.tsx
@@ -4,14 +4,16 @@ import Trans from 'next-translate/Trans'
 
 const NewReleaseBanner = () => {
   const { t } = useTranslation('home')
-  const header = [<h2 className="new-release-banner__header" />, <br />]
+  const title = [<h2 className="new-release-banner__title" />, <br />]
 
   return (
     <div className="new-release-banner">
-      <Trans i18nKey={t('newReleaseSectionHeader')} components={header} />
-      <p className="new-release-banner__description">
-        {t('newReleaseSectionDescription')}
-      </p>
+      <header className="new-release-banner__header">
+        <Trans i18nKey={t('newReleaseSectionTitle')} components={title} />
+        <p className="new-release-banner__description">
+          {t('newReleaseSectionDescription')}
+        </p>
+      </header>
     </div>
   )
 }
