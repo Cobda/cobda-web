8e5e3ab44960739a932ef07861a7b31875491fe3
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-03-10 13:21:15 +0700

    [ch133][ch137] Remove unused code

--- src/components/AboutUsSection/index.tsx
+++ src/components/AboutUsSection/index.tsx
@@ -6,7 +6,7 @@ const AboutUsSection = () => {
   return (
     <>
       <AboutUsIntroduction />
-      <section className="about-us-section about-us-section--light">
+      <section className="about-us-section about-us-section--goal">
         <header className="about-us-section__header">
           <h2 className="about-us-section__title">Our Goal</h2>
         </header>
