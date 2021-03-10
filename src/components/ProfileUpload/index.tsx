e65dfe4b51f81c62592277d34e46978315baf32c
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-03-11 00:58:41 +0700

    [ch207] Handle css when the image is selected

--- src/components/ProfileUpload/index.tsx
+++ src/components/ProfileUpload/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const ProfileUpload = () => {
-  const [selectedFile, setSelectedFile] = useState<File>()
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
   const [isFileSelected, setFileSelected] = useState<boolean>(false)
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -13,34 +13,47 @@ const ProfileUpload = () => {
   }
 
   const renderProfileImage = () => {
-    return isFileSelected ? (
+    return !isFileSelected ? (
       <img
         className="profile-upload__image"
         src="images/default-profile-image.png"
         alt="Change Profile Image"
       />
     ) : (
-      <div>Selected image</div>
+      // TODO: Use selected file instead
+      <img
+        className="profile-upload__image profile-upload__image--selected"
+        src="/images/yeezy-380.jpg"
+        alt="Change Profile Image"
+      />
+    )
+  }
+
+  const renderProfileButton = () => {
+    const buttonClassName = !isFileSelected
+      ? 'profile-upload__button'
+      : 'profile-upload__button profile-upload__button--selected'
+
+    return (
+      <label className={buttonClassName}>
+        <input
+          className="profile-upload__input"
+          type="file"
+          accept="image/png, image/jpeg"
+          onChange={handleInputChange}
+        />
+        {renderProfileImage()}
+      </label>
     )
   }
 
   return (
-    <div className="profile-upload">
-      <figure className="profile-upload__image-container">
-        <label className="profile-upload__button">
-          <input
-            className="profile-upload__input"
-            type="file"
-            accept="image/png, image/jpeg"
-            onChange={handleInputChange}
-          />
-          {renderProfileImage()}
-        </label>
-        <figcaption className="profile-upload__caption">
-          {'Maximum file size 1.5 MB'}&nbsp;&nbsp;&nbsp;&nbsp;{'JPG & PNG only'}
-        </figcaption>
-      </figure>
-    </div>
+    <figure className="profile-upload">
+      {renderProfileButton()}
+      <figcaption className="profile-upload__caption">
+        {'Maximum file size 1.5 MB'}&nbsp;&nbsp;&nbsp;&nbsp;{'JPG & PNG only'}
+      </figcaption>
+    </figure>
   )
 }
 
