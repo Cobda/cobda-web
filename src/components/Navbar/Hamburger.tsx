4b1e3f10e7e2ca9390d678e9a272cc33ac1ad2dc
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-04-05 15:47:24 +0700

    [ch167] Map hamburger menu lists into a function

--- src/components/Navbar/Hamburger.tsx
+++ src/components/Navbar/Hamburger.tsx
@@ -1,37 +1,36 @@
 import React from 'react'
 import Link from 'next/link'
+import useTranslation from 'next-translate/useTranslation'
 
 const Hamburger = () => {
+  const paths = ['/products', '/about-us', '/sign-in', '/']
+  // TODO link to sign up page when done modal //
+  const HamburgerMenuList = ['product', 'aboutUs', 'signIn', 'signUp']
+  const { t } = useTranslation('common')
+
+  const menuIcon = () => (
+    <>
+      <span></span>
+      <span></span>
+      <span></span>
+    </>
+  )
+
+  const menuItems = () =>
+    paths.map((path, index) => (
+      <li className="hamburger__menu-list">
+        <Link href={path}>
+          <a className="hamburger__menu-link">{t(HamburgerMenuList[index])}</a>
+        </Link>
+      </li>
+    ))
+
   return (
     <div className="hamburger">
       <div className="hamburger__container">
         <input type="checkbox" className="hamburger__input" />
-        <span></span>
-        <span></span>
-        <span></span>
-        <ul className="hamburger__menu">
-          <li className="hamburger__menu-list">
-            <Link href="/products">
-              <a className="hamburger__menu-link">Products</a>
-            </Link>
-          </li>
-          <li className="hamburger__menu-list">
-            <Link href="/about-us">
-              <a className="hamburger__menu-link">About Us</a>
-            </Link>
-          </li>
-          <li className="hamburger__menu-list">
-            <Link href="/sign-in">
-              <a className="hamburger__menu-link">Sign In</a>
-            </Link>
-          </li>
-          <li className="hamburger__menu-list">
-            {/* TODO link to sign up page when done modal */}
-            <Link href="">
-              <a className="hamburger__menu-link">Sign Up</a>
-            </Link>
-          </li>
-        </ul>
+        {menuIcon()}
+        <ul className="hamburger__menu">{menuItems()}</ul>
       </div>
     </div>
   )
