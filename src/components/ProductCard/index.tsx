1e0f0b105e5ab42f456ebb0d15a3136315c87fae
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-04-16 16:38:47 +0700

    [ch240][ch243] Format products layout

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -4,13 +4,16 @@ import Link from 'next/link'
 const ProductCard = () => {
   return (
     <div className="product-card">
-      <Link href="/products/sample-product-view">
-        <a className="product-card__link">
-          <div className="product-card__content">
-            <span>Click Me!</span>
-          </div>
-        </a>
-      </Link>
+      <figure className="product-card__image-container">
+        <Link href="/products/sample-product-view">
+          <a className="product-card__link">
+            <img src="/images/nike-woman-running-shoe.jpg" alt="fnwjenfioewnf" className="product-card__image" />
+          </a>
+        </Link>
+        <figcaption className="product-card__content">
+          <h2 className="product-card__title">Streetwear Socks shoes for Men's...</h2>
+        </figcaption>
+      </figure>
     </div>
   )
 }
