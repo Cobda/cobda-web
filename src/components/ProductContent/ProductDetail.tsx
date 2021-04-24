dc28903842e83274454ef5e7c67d121d0c2042cb
Raksani <raksani.k@ku.th>
2021-04-24 15:50:54 +0700

    [ch227] Add icon and initially structure all property

--- src/components/ProductContent/ProductDetail.tsx
+++ src/components/ProductContent/ProductDetail.tsx
@@ -1,20 +1,52 @@
 import React from 'react'
 
+interface ProductDetailInterface {
+  readonly title: string
+  readonly rating: Number
+  readonly price: Number
+  readonly color: string
+  readonly size: string
+  readonly time: string
+}
+
+let exampleProduct: ProductDetailInterface = {
+  title: 'Men Running Shoes Black Trainer Sport Sneaker',
+  rating: 4.8,
+  price: 560,
+  color: 'Black',
+  size: 'S,M,L',
+  time: '30 minutes'
+}
+
 const ProductDetail = () => {
   return (
     <div className="product-detail">
       <header className="product-detail__header">
-        <h2 className="product-detail__title">Shoe Title</h2>
+        <h2 className="product-detail__title">{exampleProduct.title}</h2>
       </header>
       <p className="product-detail__description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin libero
-        erat, porttitor mattis turpis eu, fermentum porttitor elit. Integer
-        auctor orci at ligula cursus pellentesque. Pellentesque in eleifend
-        ex. Curabitur bibendum orci vel magna tristique egestas. Aliquam id
-        libero augue. Proin rutrum viverra ex, ac ornare velit. Etiam accumsan
-        massa a ipsum interdum placerat. Vestibulum consequat ligula id diam
-        commodo commodo. Donec tincidunt ipsum dui, eget suscipit lorem
-        maximus sed. Integer nec diam quis erat luctus ultrices sed quis eros.
+        <div className="product-detail__rating">
+          <div className="product-detail__rating__text">{exampleProduct.rating}</div>
+          <div className="product-detail__rating__icon" />
+        </div>
+        <div className="product-detail__key">Price: {exampleProduct.price} Baht</div>
+        <div className="product-detail__key">Color: {exampleProduct.color}</div>
+        {/* Split size with buttons */}
+        <div className="product-detail__key">
+          Size:
+          <div className="product-detail__size-button"></div>
+          {exampleProduct.size}
+        </div>
+        <div className="product-detail__seller">
+          <div className="product-detail__seller__profile-picture">Picture</div>
+          <div className="product-detail__seller__username">Name</div>
+          <div className="product-detail__time-posted">posted this {exampleProduct.time} ago</div>
+        </div>
+        <div className="product-detail__delivery-option">
+          Delivery Options
+          <br />
+          <div className="product-detail__delivery-option-dropdown">Drop down</div>
+        </div>
       </p>
     </div>
   )
