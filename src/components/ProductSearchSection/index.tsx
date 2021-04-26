cb725c3287fcf67f8763d576cd043659fd28e450
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-04-26 18:25:56 +0700

    [ch240][ch243] Add filter icon

--- src/components/ProductSearchSection/index.tsx
+++ src/components/ProductSearchSection/index.tsx
@@ -1,25 +1,47 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useState } from 'react'
+import Image from 'next/image'
 import SearchBox from '../SearchBox'
+import Dropdown, { Option } from 'react-dropdown'
 import useTranslation from 'next-translate/useTranslation'
 
 const ProductSearchSection = () => {
   const { t } = useTranslation('products')
+  const [selectedFilter, setSelectedFilter] = useState<string>('')
+  const sortOption: string[] = ['highestPrice', 'lowestPrice'].map((option) => t(option))
+
+  const renderSortDropdown = () => {
+    const handleDropdownChange = (setOption: (option: string) => void) => (selectedOption: Option) => {
+      setOption(selectedOption.value)
+    }
+
+    const renderFilter = () => (
+      <div className="product-section__filter-group">
+        <Image src="/icons/filter.svg" height={21} width={24} />
+        <label className="product-section__filter-label">{t('filter')}</label>
+      </div>
+    )
+
+    return (
+      <div className="product-section__dropdown-group">
+        <label className="product-section__dropdown-label">{t('sortBy')}</label>
+        <Dropdown
+          className="product-section__dropdown-option"
+          options={sortOption}
+          value={selectedFilter}
+          placeholder="-"
+          onChange={handleDropdownChange(setSelectedFilter)}
+        />
+        {renderFilter()}
+      </div>
+    )
+  }
 
   return (
     <div className="product-section">
       <div className="product-section__search">
         <SearchBox />
       </div>
-      <div className="product-section__sort-by">
-        <span>Sorted by:</span>
-        <select className="product-section__dropdown">
-          <option>Best Match</option>
-          <option>Option 1</option>
-          <option>Option 2</option>
-          <option>Option 3</option>
-        </select>
-      </div>
+      <div className="product-section__dropdown">{renderSortDropdown()}</div>
 
       <ul className="product-section__breadcrumb">
         <li className="product-section__breadcrumb-item">
