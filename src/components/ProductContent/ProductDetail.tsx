86ebe439c8927990b20d0be84289619d2867b83c
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-04-27 17:29:09 +0700

    [ch227] Refactor code

--- src/components/ProductContent/ProductDetail.tsx
+++ src/components/ProductContent/ProductDetail.tsx
@@ -1,50 +1,47 @@
+import useTranslation from 'next-translate/useTranslation'
 import React from 'react'
 
-interface ProductDetailInterface {
+interface ProductDetail {
   readonly title: string
-  readonly rating: Number
-  readonly price: Number
+  readonly price: number
   readonly color: string
   readonly size: string
-  readonly time: string
+  readonly deliveryOption: string
 }
 
-let exampleProduct: ProductDetailInterface = {
+const exampleProduct: ProductDetail = {
   title: 'Men Running Shoes Black Trainer Sport Sneaker',
-  rating: 4.8,
-  price: 560,
-  color: 'Black',
-  size: 'S,M,L',
-  time: '30 minutes'
+  price: 5600,
+  color: 'Blue',
+  size: '42',
+  deliveryOption: 'Postal'
 }
+
+const COMMA_REGEX = new RegExp(/\B(?=(\d{3})+(?!\d))/g)
 
 const ProductDetail = () => {
+  const { t } = useTranslation('product-view')
+  // TODO: Remove this mock
+  const { title, price, color, size, deliveryOption } = exampleProduct
+  
+  const formattedPrice = price.toString().replace(COMMA_REGEX, ',')
+  const priceLabel: string = t('price') + formattedPrice + ' ' + t('baht')
+  const colorLabel: string = t('color') + color
+  const sizeLabel: string = t('size') + size
+  const deliveryOptionLabel: string = t('deliveryOption') + deliveryOption
+  const labels: string[] = [priceLabel, colorLabel, sizeLabel, deliveryOptionLabel]
+
+  const renderProductLabels = () => labels.map((label) => <h3 className="product-detail__label">{label}</h3>)
+
   return (
     <div className="product-detail">
       <header className="product-detail__header">
-        <h2 className="product-detail__title">{exampleProduct.title}</h2>
+        <h2 className="product-detail__title">{title}</h2>
       </header>
-      <p className="product-detail__description">
-        <div className="product-detail__rating">
-          <div className="product-detail__rating__text">{exampleProduct.rating}</div>
-          <div className="product-detail__rating__icon" />
-        </div>
-        <div className="product-detail__key">Price: {exampleProduct.price} Baht</div>
-        <div className="product-detail__key">Color: {exampleProduct.color}</div>
-        <div className="product-detail__key">Size: {exampleProduct.size}</div>
-        {/* TimeStamp */}
-        <div className="product-detail__seller">
-          <div className="product-detail__seller__profile-picture"></div>
-          <div className="product-detail__seller__username">John Wick</div>
-          <div className="product-detail__time-posted">posted this {exampleProduct.time} ago</div>
-        </div>
-        <div className="product-detail__key">
-          Delivery Options
-          <br />
-        </div>
-        <div className="product-detail__delivery-option-dropdown">Drop down</div>
-      </p>
-      <button className="product-detail__button">Contact Seller</button>
+      <div className="product-detail__body">{renderProductLabels()}</div>
+      <div className="product-detail__footer">
+        <button className="product-detail__button">{t('contactSeller')}</button>
+      </div>
     </div>
   )
 }
