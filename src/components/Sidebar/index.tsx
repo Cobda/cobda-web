a77d7e993c25198da3f177ecf773e108408be991
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-04-27 17:18:00 +0700

    [ch234] Create function rendering filter item

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,58 @@
 import React from 'react'
+import useTranslation from 'next-translate/useTranslation'
 
 const Sidebar = () => {
+  const { t } = useTranslation('products')
+
+  const renderCategoryItem = () => (
+    <div className="sidebar__item">
+      <div className="sidebar__item-group">
+        <input className="sidebar__checkbox" type="checkbox"></input>
+        <label className="sidebar__label">{t('shoe')}</label>
+      </div>
+      <div className="sidebar__item-group">
+        <input className="sidebar__checkbox" type="checkbox"></input>
+        <label className="sidebar__label">{t('shirt')}</label>
+      </div>
+    </div>
+  )
+
+  const renderPriceRangeItem = () => (
+    <div className="sidebar__item">
+      <div className="sidebar__item-group">
+        <input className="sidebar__checkbox" type="checkbox"></input>
+        <label className="sidebar__label">0 - 5000 {t('baht')}</label>
+      </div>
+      <div className="sidebar__item-group">
+        <input className="sidebar__checkbox" type="checkbox"></input>
+        <label className="sidebar__label">5000 - 10,000 {t('baht')}</label>
+      </div>
+      <div className="sidebar__item-group">
+        <input className="sidebar__checkbox" type="checkbox"></input>
+        <label className="sidebar__label">10,000 + {t('baht')}</label>
+      </div>
+    </div>
+  )
   const renderCategorySection = () => (
-    <section className="sidebar__category">
-      <header className="sidebar__category-title">Category</header>
-      <input className="sidebar__checkbox" type="checkbox"></input>
-      <label> Men's clothing</label>
+    <section className="sidebar__filter">
+      <header className="sidebar__title">{t('category')}</header>
+      {renderCategoryItem()}
     </section>
   )
 
-  const renderColorSection = () => <div className="sidebar__color"></div>
+  const renderPriceRange = () => (
+    <section className="sidebar__filter">
+      <header className="sidebar__title">{t('priceRange')}</header>
+      {renderPriceRangeItem()}
+    </section>
+  )
 
-  const renderSizeSection = () => <div className="sidebar__size"></div>
-
-  const renderPriceRange = () => <div className="sidebar__price-range"></div>
-
-  return <div className="sidebar">{renderCategorySection()}</div>
+  return (
+    <div className="sidebar">
+      {renderCategorySection()}
+      {renderPriceRange()}
+    </div>
+  )
 }
 
 export default Sidebar
