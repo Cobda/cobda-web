5326a2dc286e6847f1401c0107fce45bd03c4548
bankkeez <sethitikul@gmail.com>
2021-04-27 22:11:41 +0700

    Adjust home search section

--- src/components/HomeCategorySection/index.tsx
+++ src/components/HomeCategorySection/index.tsx
@@ -6,7 +6,7 @@ const HomeCategorySection = () => {
   const { t } = useTranslation('home')
 
   return (
-    <section className="home-section home-section--dark">
+    <section className="home-section">
       <header className="home-section__header">
         <h2 className="home-section__title">{t('categorySectionTitle')}</h2>
       </header>
