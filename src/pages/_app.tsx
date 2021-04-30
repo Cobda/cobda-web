951432a87c02c34516368e430967ee61c08b0719
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-05-01 01:19:18 +0700

    [ch276] Add loading to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppProps } from 'next/app'
 import '../styles/main.scss'
 import Meta from '../components/Meta'
 import { Provider } from 'next-auth/client'
 import { RecoilRoot } from 'recoil'
+import { useRouter } from 'next/router'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return (
-    <RecoilRoot>
-      <Provider session={pageProps.session}>
-        <Meta />
-        <Component {...pageProps} />
-      </Provider>
-    </RecoilRoot>
-  )
+  const [isLoading, setLoading] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteStart = () => {
+      setLoading(true)
+    }
+
+    const handleRouteEnd = () => {
+      setLoading(false)
+    }
+
+    router.events.on('routeChangeStart', handleRouteStart)
+    router.events.on('routeChangeComplete', handleRouteEnd)
+    router.events.on('routeChangeError', handleRouteEnd)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteStart)
+      router.events.off('routeChangeComplete', handleRouteEnd)
+      router.events.off('routeChangeError', handleRouteEnd)
+    }
+  }, [])
+
+  const renderAppComponent = () =>
+    isLoading ? (
+      <img className="loading" src="/icons/loading.svg" alt="Loading Image" />
+    ) : (
+      <RecoilRoot>
+        <Provider session={pageProps.session}>
+          <Meta />
+          <Component {...pageProps} />
+        </Provider>
+      </RecoilRoot>
+    )
+
+  return renderAppComponent()
 }
 
 export default App
