b03199c7a61d2971b8de157eceea55329d609708
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-05-03 02:03:24 +0700

    [ch276] Display empty product result

--- src/components/ProductListBody/index.tsx
+++ src/components/ProductListBody/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ProductCard from '../ProductCard'
 import { useRecoilValue } from 'recoil'
 import { filteredProductListState } from '../../recoil/selectors'
+import EmptyResult from '../EmptyResult'
 
 const ProductListBody = () => {
   const productList = useRecoilValue(filteredProductListState)
 
-  const renderProductList = () => productList?.map((product) => <ProductCard product={product} />)
+  const renderProductList = () =>
+    productList?.length ? productList.map((product) => <ProductCard product={product} />) : <EmptyResult />
 
   return <section className="product-list">{renderProductList()}</section>
 }
