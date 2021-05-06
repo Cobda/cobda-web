c638222b373c2b95c9421a51a90d12e1dfcd4e53
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-05-06 23:38:57 +0700

    [ch276] Handle redirection when there is no user

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from '../components/Footer'
 import Meta from '../components/Meta'
 import Navbar from '../components/Navbar'
 import AccountContent from '../components/AccountContent'
 import AccountInformation from '../components/AccountInformation'
+import { useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 
-const Account = () => (
-  <div className="layout-account">
-    {/* TODO: Title and file name should be username instead. Ex: Shawn Mentos | Cobda */}
-    <Meta title="Account | Cobda" />
-    <header>
-      <Navbar />
-    </header>
-    {/* TODO: For further implemetation}
+const Account = () => {
+  const [session] = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!session) {
+      router.push('/sign-in')
+    }
+  }, [])
+
+  return (
+    <div className="layout-account">
+      {/* TODO: Title and file name should be username instead. Ex: Shawn Mentos | Cobda */}
+      <Meta title="Account | Cobda" />
+      <header>
+        <Navbar />
+      </header>
+      {/* TODO: For further implemetation}
     {/* <aside>
       <AccountInformation />
     </aside> */}
-    <main>
-      <AccountContent />
-    </main>
-    <footer>
-      <Footer />
-    </footer>
-  </div>
-)
+      <main>
+        <AccountContent />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
 
 export default Account
