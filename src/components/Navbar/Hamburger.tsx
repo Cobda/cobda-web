fc56254d4565180410c35e09713b812934de9004
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2021-05-17 17:32:28 +0700

    [ch278] Handle account redirect

--- src/components/Navbar/Hamburger.tsx
+++ src/components/Navbar/Hamburger.tsx
@@ -11,18 +11,20 @@ const NavbarHamburger = () => {
   const [session] = useSession()
   const { t } = useTranslation('common')
   const pathsWithoutSession: string[] = ['/products', '/about-us', '/product-registration', '/sign-in', '/sign-up']
-  const pathsWithSession: string[] = ['/products', '/about-us', '/product-registration', '/profile', '/']
+  const pathsWithSession: string[] = ['/products', '/about-us', '/product-registration', '/account', '/']
   const menuItemsWithoutSession: string[] = ['product', 'aboutUs', 'sell', 'signIn', 'signUp']
-  const menuItemsWithSession: string[] = ['product', 'aboutUs', 'sell', 'profile', 'signOut']
+  const menuItemsWithSession: string[] = ['product', 'aboutUs', 'sell', 'account', 'signOut']
   const menuItems: string[] = session ? menuItemsWithSession : menuItemsWithoutSession
   const paths: string[] = session ? pathsWithSession : pathsWithoutSession
 
   const handleLinkClick = (path: string) => () => {
     if (path === '/') {
       signOut()
+    } else if (path === '/account') {
+      router.push({ pathname: `/account`, query: { id: session?.user.id as string } })
+    } else {
+      router.push(path)
     }
-    
-    router.push(path)
   }
 
   const renderHamburgerIcon = () => (
